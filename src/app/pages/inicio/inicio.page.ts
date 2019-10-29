a9964ebcdff821b5d499192d6177d5091c9b254a
Carlos <cavillarroel11@gmail.com>
2019-10-29 10:41:35 -0300

    Ion Menu Implemented with Json and Data Service

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Componente } from '../../interfaces/interfaces';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inicio',
@@ -7,101 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  componentes: Componente[] = [
-    {
-      icon: 'american-football',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: 'appstore',
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-    {
-      icon: 'beaker',
-      name: 'Avatar',
-      redirectTo: '/avatar'
-    }
-    ,
-    {
-      icon: 'radio-button-on',
-      name: 'Botones',
-      redirectTo: '/botones'
-    }
-    ,
-    {
-      icon: 'card',
-      name: 'Cards',
-      redirectTo: '/card'
-    }
-    ,
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Check',
-      redirectTo: '/check'
-    }
-    ,
-    {
-      icon: 'calendar',
-      name: 'DateTime',
-      redirectTo: '/date-time'
-    }
-    ,
-    {
-      icon: 'car',
-      name: 'Fab',
-      redirectTo: '/fab'
-    }
-    ,
-    {
-      icon: 'grid',
-      name: 'Grid - Rows',
-      redirectTo: '/grid'
-    }
-    ,
-    {
-      icon: 'infinite',
-      name: 'Infinite - Scroll',
-      redirectTo: '/infinite-scroll'
-    }
-    ,
-    {
-      icon: 'hammer',
-      name: 'Input',
-      redirectTo: '/input'
-    }
-    ,
-    {
-      icon: 'list',
-      name: 'Listas - Sliding',
-      redirectTo: '/list'
-    }
-    ,
-    {
-      icon: 'reorder',
-      name: 'Listas - Reorder',
-      redirectTo: '/list-reorder'
-    }
-    ,
-    {
-      icon: 'refresh-circle',
-      name: 'Loading',
-      redirectTo: '/loading'
-    }
+  componentes: Observable<Componente[]>;
 
-
-  ];
-  constructor() { }
+  constructor(
+    private menuCtrl: MenuController,
+    private dataService: DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
   }
 
-}
 
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
+  toggleMenu() {
+    this.menuCtrl.toggle();
+  }
 
 }
